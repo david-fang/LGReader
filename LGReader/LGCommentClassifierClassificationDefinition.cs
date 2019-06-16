d87a3d24cb8c5cb337b9e1324be69606d811be73
David Fang <dafang@microsoft.com>
2019-06-16 16:03:46 -0700

    Add skeleton files for comment classification

---
+++ LGReader/LGCommentClassifierClassificationDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace LGReader
+{
+    /// <summary>
+    /// Classification type definition export for LGCommentClassifier
+    /// </summary>
+    internal static class LGCommentClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "LGCommentClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("LGCommentClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ LGReader/LGCommentClassifierFormat.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace LGReader
+{
+    /// <summary>
+    /// Defines an editor format for the LGCommentClassifier type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "LGCommentClassifier")]
+    [Name("LGCommentClassifier")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    internal sealed class LGCommentClassifierFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LGCommentClassifierFormat"/> class.
+        /// </summary>
+        public LGCommentClassifierFormat()
+        {
+            this.DisplayName = "LGCommentClassifier"; // Human readable version of the name
+            this.BackgroundColor = Colors.BlueViolet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+}
